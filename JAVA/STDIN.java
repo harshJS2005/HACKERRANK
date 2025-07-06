5409cf4b9be84f942e6941fb7aaa14c99d3d09b8
Harsh Srivastava <harsh.js2005@gmail.com>
2025-07-06 20:40:45 +0530

    Update STDIN.java

--- JAVA/STDIN.java
+++ JAVA/STDIN.java
@@ -1,4 +1,5 @@
-/* Task
+/* Task                                                    JULY 5 , 2025
+
 In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
 
 -> Input Format:
