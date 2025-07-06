108b8ff043b766a7a1f1ffbe843b83b0dc4f553e
Harsh Srivastava <harsh.js2005@gmail.com>
2025-07-06 20:41:09 +0530

    Update STDIN_STDOUT-II.java

--- JAVA/STDIN_STDOUT-II.java
+++ JAVA/STDIN_STDOUT-II.java
@@ -1,4 +1,5 @@
-/* 
+/*                                                             JULY 5 , 2025
+
 In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below.
 
 -> Input Format
